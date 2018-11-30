f19f8de2006cd102eff8f33bd8ce88c0e3557c74
zitt <NKtargeting@yandex.ru>
2018-11-30 17:38:33 +0500

    Implement the monix worker

--- src/main/scala/ru/tinkoff/fintech/homework09/monix/crawler/Worker.scala
+++ src/main/scala/ru/tinkoff/fintech/homework09/monix/crawler/Worker.scala
@@ -5,5 +5,21 @@ trait Worker {
   def http: Http
   def parseLinks: Parsr
 
-  def worker(workerQueue: MQueue[Url], crawlerQueue: MQueue[CrawlerMessage]): Task[Fiber[Unit]] = ???
+  def worker(workerQueue: MQueue[Url], crawlerQueue: MQueue[CrawlerMessage]): Task[Fiber[Unit]] = {
+    def doWork(task: Task[Url]): Task[Unit] = {
+      task.flatMap { url =>
+        http.get(url).map { body =>
+          parseLinks.links(body)
+        }.onErrorRecover {
+          case _: Exception => Nil
+        }.flatMap { links =>
+          crawlerQueue.offer(CrawlResult(url, links))
+        }.flatMap { _ =>
+          doWork(workerQueue.take)
+        }
+      }
+    }
+
+    doWork(workerQueue.take).start
+  }
 }
