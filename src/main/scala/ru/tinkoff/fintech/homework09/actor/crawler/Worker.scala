83e71201618489e9a16fe7ba08197d3657562810
zitt <NKtargeting@yandex.ru>
2018-11-30 14:37:52 +0500

    Implement the actor crawler

--- src/main/scala/ru/tinkoff/fintech/homework09/actor/crawler/Worker.scala
+++ src/main/scala/ru/tinkoff/fintech/homework09/actor/crawler/Worker.scala
@@ -1,6 +1,34 @@
 package ru.tinkoff.fintech.homework09.actor.crawler
 import akka.actor.{Actor, ActorLogging, ActorRef}
 
+import scala.collection.mutable
+import scala.util.{Failure, Success}
+
 class Worker(http: Http, parser: Parsr, master: ActorRef) extends Actor with ActorLogging {
-  override def receive: Receive = ???
+  implicit val ec = context.dispatcher
+  private var isWorking = false
+  private val queue = mutable.Queue.empty[Url]
+
+  private def sendToMaster(url: Url, result: List[Url]): Unit = {
+    master ! CrawlResult(url, result)
+    isWorking = false
+    work()
+  }
+
+  private def work(): Unit =
+    if (!isWorking && queue.nonEmpty) {
+      isWorking = true
+      val url = queue.dequeue()
+
+      http.get(url).map(b => parser.links(b)).onComplete {
+        case Success(urls) => sendToMaster(url, urls)
+        case Failure(_)    => sendToMaster(url, Nil)
+      }
+    }
+
+  override def receive: Receive = {
+    case Crawl(url) =>
+      queue.enqueue(url)
+      work()
+  }
 }
